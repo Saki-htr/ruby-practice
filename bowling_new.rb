ce71d4f081414f33a715eb8f0ef0fb70e9bc5ee7
Saki-htr <saki.hattori0116@gmail.com>
2024-10-03 23:52:41 +0900

    update: スコア計算の処理まで全部完了

--- bowling_new.rb
+++ bowling_new.rb
@@ -4,21 +4,32 @@ def calc_score(pinfall_text)
   pinfalls = parse_pinfall_text(pinfall_text)
   # スコアをフレームごとの2次元配列に変換する
   frames = [] # これに格納する
-  pinfalls.each_with_index do |pinfall, index|
+  pinfalls.each_with_index.sum do |pinfall, index|
     # 1-9frameは[]の中に入れて、 10frame目は[]をたさず、全部同じ[]に入れる
     frames << [] if next_frame?(frames)
+    #=> frames: [[6, 3], [9, 0], [0, 3], [8, 2], [7, 3], [10], [9, 1], [8, 0], [10], [6, 4, 5]]
     rolls = frames.last
     rolls << pinfall
+    # add_bonusメソッドで使う用の変数
+    following_pinfalls = pinfalls[(index + 1)..-1] # 今見てる投球の点数の直後〜一番最後の投球までを入れてる
+    last_frame?(frames) ? pinfall : pinfall + add_bonus(rolls, following_pinfalls)
   end
-  p frames
 end
 
+def add_bonus(rolls, following_pinfalls)
+  if strike?(rolls)
+    following_pinfalls.first(2).sum
+  elsif spare?(rolls)
+    following_pinfalls.first
+  else
+    0
+  end
+end
 
 # 引数で受け取った文字列を配列入りの数字に変換(xも10へ)
 def parse_pinfall_text(pinfall_text)
   pinfall_text.split(',').map { |c| c == 'X' ? 10 : c.to_i}
 end
-
 
 # 次のフレームが存在するか判定(なければ次のフレーム[]を作れない)
 def next_frame?(frames)
@@ -35,4 +46,10 @@ def strike?(rolls)
   rolls[0] == 10
 end
 
-calc_score('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5')
+def spare?(rolls)
+  !strike?(rolls) && rolls.sum == 10
+end
+
+if __FILE__ == $PROGRAM_NAME
+  puts calc_score(ARGV[0])
+end
